3a55c50f273f5c68e58fbc924d7911a5af22ae39
John Mckay <adenosine3p@gmail.com>
2019-11-09 19:52:11 +0000

    even more merge fixes

--- src/mumble/JackAudio.cpp
+++ src/mumble/JackAudio.cpp
@@ -867,7 +867,7 @@ bool JackAudioOutput::process(const jack_nframes_t &frames) {
 			return false;
 		}
 
-		outputBuffers.replace(currentChannel, dynamic_cast<jack_default_audio_sample_t*>(outputBuffer));
+		outputBuffers.replace(currentChannel, reinterpret_cast<jack_default_audio_sample_t*>(outputBuffer));
 	}
 
 	const size_t needed = frames * iSampleSize;
